958079efd8d2f8fa4e7a15f1aee9697d55d7694c
marian <rahman.marian@gmail.com>
2019-02-03 18:53:12 -0500

    updated bamazonCustomer.js.

--- bamazonCustomers.js
+++ bamazonCustomers.js
@@ -1,38 +1,30 @@
-
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var cTable = require("console.table");
 
 var db = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  username: "root",
+  user: "root",
   password: "root",
   database: "bamazon_db"
 });
 
 db.connect(function (err) {
-
   if (err) throw err;
-  console.log("connected as id: ", connection.threadId);
-  itemForSale();
-});
 
-
-db.connect(function (err) {
-
-  if (err) throw err;
-  console.log("connected as id: ", connection.threadId);
+  console.log("connected as id: ", db.threadId);
   itemForSale();
 });
 
 function itemForSale() {
-  
+
   let queryDB = "SELECT * FROM products";
 
   db.query(queryDB, function (err, result) {
-    if(err) throw err;
+    if (err) throw err;
 
-    console.log(result);
+    console.table(result);
 
   })
   purchase();
@@ -40,33 +32,40 @@ function itemForSale() {
 
 function purchase() {
   inquirer
-  .prompt([
-    {
-      name: "item",
-      type: "input",
-      message: "What item ID would you like to purschase?"
-    },
-    {
-      name: "units",
-      type: "input",
-      message: "How many units would you like?"
-    }
-  ])
-  .then(function (ans) {
-    var queryDB = "SELECT * FROM product WHERE item_id = ?";
-    
-  db.query(querydb, ans.item, function (err, result) {
-    if (!result.length) {
-      
-      console.log("\r\n");
-      console.log("Sorry this item is not available, please select another item.");
-      purchase();
+    .prompt([{
+        name: "item",
+        type: "input",
+        message: "What item ID would you like to purschase?"
+      },
+      {
+        name: "units",
+        type: "input",
+        message: "How many units would you like?"
+      }
+    ])
+    .then(function (ans) {
+      var queryDB = "SELECT * FROM product WHERE item_id = ?";
 
-    }
-        else{
-          if(ans.unit > result[0])
-          }
+      db.query(querydb, ans.item, function (err, result) {
+        if (!result.length) {
+
+          console.log("\r\n");
+          console.log("Sorry this item is not available, please select another item.");
+          purchase();
+
+        } else if (ans.units > result[0].stock_quantity) {
+
+          console.log("\r\n Sorry, we do not have that amount.  Please try again \r\n");
+
+        } else {
+          let updateDB = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
+          let newDbAmount = result[0].stock_quantity - ans.units;
+
+          db.query(updateDB, [newDbAmount, ans.item], function (err, res) {
+            console.log("\r\n Thank you for purchase.  Come again soon.\r\n");
+            db.end();
+          })
         }
-  })
-  })
-}
+      })
+    })
+};
